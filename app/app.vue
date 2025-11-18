0676935fd2061347ae74f9e6cadfb9b9a88227de
Irving A. Bermúdez S. <bigchirv@gmail.com>
2025-11-17 23:30:23 -0500

    feat: enhance Hero section with smooth scrolling and improved accessibility

--- app/app.vue
+++ app/app.vue
@@ -32,7 +32,8 @@ useHead({
   title: 'Irving A. Bermúdez S. - Full-Stack Engineer & Systems Architect',
   htmlAttrs: {
     lang: computed(() => lang.value),
-    "data-theme": "nord"
+    "data-theme": "nord",
+    style: "scroll-behavior: smooth;"
   },
   meta: [
     { name: 'description', content: 'Personal portfolio website for Irving Bermudez, a Full-Stack Engineer and Systems Architect with over 20 years of experience.' }
--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -3,9 +3,10 @@
     <div class="hero-content text-left flex-col items-start">
       <h1 class="text-2xl mb-10">{{ heroGreeting }}</h1>
       <div class="carousel w-6xl">
-        <div div v-for="content, index in heroContent" :key="index" class="carousel-item w-6xl">
+        <div div v-for="content, index in heroContent" :key="index" :id="'hero-item-' + index"
+          class="carousel-item w-6xl">
           <div>
-            <h2 class="text-6xl font-semibold mb-10">{{ content.headline }}</h2>
+            <h2 class="text-6xl font-bold mb-10">{{ content.headline }}</h2>
             <p class="text-2xl mb-10">{{ content.subheadline }}</p>
             <div class="flex mt-10 gap-2 justify-center">
               <a href="#projects" class="btn btn-primary">{{ content.cta.primary }}</a>
@@ -14,12 +15,18 @@
           </div>
         </div>
       </div>
-      <div class="flex gap-2 justify-center w-full">
-        <a :href="heroLinks.github" target="_blank" rel="noopener noreferrer"
-          class="text-dark hover:text-primary mx-4">GitHub</a>
-        <a :href="heroLinks.linkedin" target="_blank" rel="noopener noreferrer"
-          class="text-dark hover:text-primary mx-4">LinkedIn</a>
-        <a :href="'mailto:' + heroLinks.email" class="text-dark hover:text-primary mx-4">Email</a>
+      <div class="flex w-full justify-center gap-2 py-2">
+        <a div v-for="content, index in heroContent" :key="index" :href="'#hero-item-' + index"
+          class="btn btn-xs btn-circle"></a>
+      </div>
+      <div>
+        <div class="flex gap-2 justify-center w-full">
+          <a :href="heroLinks.github" target="_blank" rel="noopener noreferrer"
+            class="text-dark hover:text-primary mx-4">GitHub</a>
+          <a :href="heroLinks.linkedin" target="_blank" rel="noopener noreferrer"
+            class="text-dark hover:text-primary mx-4">LinkedIn</a>
+          <a :href="'mailto:' + heroLinks.email" class="text-dark hover:text-primary mx-4">Email</a>
+        </div>
       </div>
     </div>
   </section>
